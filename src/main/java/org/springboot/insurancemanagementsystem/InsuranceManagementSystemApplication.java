d55703c6fcbe413eec0899411b5d5f2342260dd3
milendrakumarbaghel <baghelmilendrakumar@gmail.com>
2026-06-03 16:31:52 +0530

    first commit

---
+++ src/main/java/org/springboot/insurancemanagementsystem/InsuranceManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package org.springboot.insurancemanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InsuranceManagementSystemApplication.class, args);
+    }
+
+}
