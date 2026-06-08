48575a9406115f8830b25d654f86ab9547cc6bf2
Amitesh Kumar <amitesh11130@gmail.com>
2026-06-08 21:20:11 +0530

    Adding validation in request dtos

--- src/main/java/org/springboot/insurancemanagementsystem/dto/ClaimReviewRequestDto.java
+++ src/main/java/org/springboot/insurancemanagementsystem/dto/ClaimReviewRequestDto.java
@@ -1,5 +1,9 @@
 package org.springboot.insurancemanagementsystem.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter
@@ -10,7 +14,14 @@ import lombok.*;
 @Builder
 public class ClaimReviewRequestDto {
 
+    @NotNull(message = "Recommendation status is required (true/false)")
     private Boolean recommended;
 
+    @NotBlank(message = "Review remarks are required")
+    @Size(min = 5, max = 500, message = "Remarks must be between 5 and 500 characters")
+    @Pattern(
+            regexp = "^[^<>]*$",
+            message = "Remarks cannot contain HTML tags or script character sequences (< or >)"
+    )
     private String remarks;
 }
